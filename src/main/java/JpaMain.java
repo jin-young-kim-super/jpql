f4154e32b7b66ec6fce2f29593314809b7503d5c
Jin Young Kim <wlsdud66523@kakao.com>
2026-09-08 06:52:52 +0900

    PROJECTION

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -1,5 +1,8 @@
 import jakarta.persistence.*;
+import jpql.Address;
 import jpql.Member;
+import jpql.MemberDto;
+import jpql.Team;
 
 import java.util.List;
 
@@ -18,28 +21,38 @@ public class JpaMain {
             member.setAge(10);
             entityManager.persist(member);
 
-            // 2번째 파라미터 : 응답 객체 타입 지정
-            // -> 응답 객체 타입은 PROJECT에서 나중에 자세히 배움
-            TypedQuery<Member> typedQuery = entityManager.createQuery("select m from Member m", Member.class); // TypeQuery : 응답 객체가 명확할 떄!
-            Query query = entityManager.createQuery("select m.username, m.age from Member m"); // Query : 응답 객체가 명확하지 않을 떄!
+            entityManager.flush();
+            entityManager.clear();
 
+            // 엔티티 프로젝션 : 반드시 영속성 컨텍스트에 관리
+            List<Member> findMembers = entityManager.createQuery("select m from Member m", Member.class)
+                    .getResultList();
 
-            // getResultList() : List로 반환되며 결과가 여러 개 반환될 떄!
-            List<Member> selectMFromMemberM = entityManager.createQuery("select m from Member m", Member.class).getResultList();
+            // 엔티티 프로젝션 : 반드시 영속성 컨텍스트에 관리
+            // -> 메모 참고!!!!
+            List<Team> findTeams = entityManager.createQuery("select m.team from Member m", Team.class)
+                    .getResultList();
 
-            // getSingleResult() : 결과가 1개만 반환될 떄!
-            Member singleResult = entityManager.createQuery("select m from Member m where m.id = '10'", Member.class).getSingleResult();
+            // 임베디드 프로젝션 : 영속성 컨텍스트에 관리x
+            List<Address> findAddress = entityManager.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
 
+            // 스칼라 타입 프로젝션 : 영속성 컨텍스트에 관리x
+            // -> 메모 참고.
+            List resultList = entityManager.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
 
-            // 파라미터 바인딩 : 이름 기준
-            TypedQuery<Member> parameterQuery = entityManager.createQuery("select m from Member m where m.username= :username", Member.class);
-            parameterQuery.setParameter("username","member1");
-            Member singRst = parameterQuery.getSingleResult();
+            Object o = resultList.get(0); // DB 결과의 첫 행
+            Object[] result = (Object[]) o; // 업 캐스팅
+            System.out.println("username=" + result[0]);
+            System.out.println("age = " + result[1]);
 
-            // 파라미터 바인딩 : 위치 기준(절대 사용하지  말것)
-            TypedQuery<Member> parameterQuery2 = entityManager.createQuery("select m from Member m where m.username=?1", Member.class);
-            parameterQuery2.setParameter(1,"kim"); // 만약에 where절에 바인딩 파라미터가 추가가 되거나, 하면 setParameter시 위치 1,2,3 등의 오기입이 생길 수 있어서 장애로 이어진다. 이름 기반은 그런 오류는 안 일어 난다.
-            Member singleResult1 = parameterQuery2.getSingleResult();
+            // selct절에 여러 개의 값을 projection하는 경우 젤 깔끔하는 방법
+            List<MemberDto> resultList1 = entityManager.createQuery("select new jpql.MemberDto(m.username,m.age) from Member m", MemberDto.class)
+                    .getResultList();
+            for (MemberDto memberDto : resultList1) {
+                System.out.println("memberDto = " + memberDto);
+            }
 
 
             transaction.commit();
