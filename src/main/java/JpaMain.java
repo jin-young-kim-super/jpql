7f9b3990e39afc0c4881c7d45cad3cb1645c6e14
Jin Young Kim <wlsdud66523@kakao.com>
2026-09-10 05:58:46 +0900

    条件式(CASE など)

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -25,20 +25,39 @@ public class JpaMain {
             member.changeTeam(team);
             entityManager.persist(member);
 
-            //  JPQL에서 문자열 : 싱글 따옴표
-            //  JPQL에서 불린형 : TRUE, FALSE, true, false
-            //  JPAQL에서 ENUM : 패키지 경로를 전부 명시
-            String jpql = "select m.username, 'HELLO', TRUE FROM Member m WHERE m.memberType = jpql.MemberType.ADMIN";
-            List<Object[]> resultList = entityManager.createQuery(jpql).getResultList();
+            // 기본 case문(변수가 when문 안에 있는 것)
+            String jpql1 = "select " +
+                                     "case when m.age <= 10 then '학생요금'"+
+                                     "     when m.age >= 60 then '경로요금'"+
+                                     "     else '일반요금'"+
+                                     "end" +
+                          " from Member as m";
+            List<String> resultList1 = entityManager.createQuery(jpql1, String.class)
+                    .getResultList();
 
-            /**
-             * ENUM의 긴 패키지명이 짜는 날 경우 해결법
-             * -> 바인딩으로 해결
-             */
-//            String jpql = "select m.username, 'HELLO', TRUE FROM Member m WHERE m.memberType = :memberType";
-//            List<Object[]> resultList = entityManager.createQuery(jpql)
-//                    .setParameter("memberType",MemberType.ADMIN)
-//                    .getResultList();
+            // 단순 case문(변수가 when문 밖에 있는 것, switch-case랑 비슷)
+            String jpql2 = "select " +
+                                    "case t.name  " +
+                                    "     when '맨유' then '인센티브110%'"+
+                                    "     when '토트넘' then '인센티브120%'"+
+                                    "     else '인센티브100%'"+
+                                    "end" +
+                           " from Team as t";
+            List<String> resultList2 = entityManager.createQuery(jpql2, String.class)
+                    .getResultList();
+
+            // COALESCE문 : 모든 레코드를 하나씪 조회해서 null인 경우 지정값 반환, null이 아닌 경우 해당 컬럼 반환
+            String jpql3 = "select COALESCE(m.username,'이름 없는 회원') as username" +
+                           " from Member as m";
+            List<String> resultList3 = entityManager.createQuery(jpql3, String.class)
+                    .getResultList();
+
+
+            // NULLIF문 : 조건문 충족 시 NULL 반환
+            String jpql4 = "select NULLIF(m.username,'이름 있는 회원') as username" +
+                    " from Member as m";
+            List<String> resultList4 = entityManager.createQuery(jpql4, String.class)
+                    .getResultList();
 
 
             entityManager.flush();
