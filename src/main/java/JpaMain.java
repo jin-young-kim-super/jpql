035e33f78bafc61d9c3b5409238255b0c621f332
Jin Young Kim <wlsdud66523@kakao.com>
2026-09-10 06:49:00 +0900

    JPQL 関数

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -25,40 +25,10 @@ public class JpaMain {
             member.changeTeam(team);
             entityManager.persist(member);
 
-            // 기본 case문(변수가 when문 안에 있는 것)
-            String jpql1 = "select " +
-                                     "case when m.age <= 10 then '학생요금'"+
-                                     "     when m.age >= 60 then '경로요금'"+
-                                     "     else '일반요금'"+
-                                     "end" +
-                          " from Member as m";
-            List<String> resultList1 = entityManager.createQuery(jpql1, String.class)
+            // JPA 내장 함수 사용 예시
+            String jpql = "select concat('a','b') From Member as m";
+            List<String> resultList = entityManager.createQuery(jpql, String.class)
                     .getResultList();
-
-            // 단순 case문(변수가 when문 밖에 있는 것, switch-case랑 비슷)
-            String jpql2 = "select " +
-                                    "case t.name  " +
-                                    "     when '맨유' then '인센티브110%'"+
-                                    "     when '토트넘' then '인센티브120%'"+
-                                    "     else '인센티브100%'"+
-                                    "end" +
-                           " from Team as t";
-            List<String> resultList2 = entityManager.createQuery(jpql2, String.class)
-                    .getResultList();
-
-            // COALESCE문 : 모든 레코드를 하나씪 조회해서 null인 경우 지정값 반환, null이 아닌 경우 해당 컬럼 반환
-            String jpql3 = "select COALESCE(m.username,'이름 없는 회원') as username" +
-                           " from Member as m";
-            List<String> resultList3 = entityManager.createQuery(jpql3, String.class)
-                    .getResultList();
-
-
-            // NULLIF문 : 조건문 충족 시 NULL 반환
-            String jpql4 = "select NULLIF(m.username,'이름 있는 회원') as username" +
-                    " from Member as m";
-            List<String> resultList4 = entityManager.createQuery(jpql4, String.class)
-                    .getResultList();
-
 
             entityManager.flush();
             entityManager.clear();
